20312096cfe854673f6bf7f2b2ca1a47ee163695
杉思 <sansi.xy@antgroup.com>
2026-01-05 14:26:51 +0800

    fix

---
+++ src/main/java/com/alipay/utils/AlgorithmUtil.java
@@ -0,0 +1,95 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.alipay.utils;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.IntStream;
+
+import static com.alipay.utils.DoubleUtil.round;
+
+public class AlgorithmUtil {
+    public static double[] maxSlidingWindow(double[] nums, int size) {
+        if (nums != null && nums.length > 0 && size > 0) {
+            LinkedList<Integer> queue = Lists.newLinkedList();
+            double[] result = new double[nums.length - size + 1];
+
+            for(int i = 0; i < nums.length; ++i) {
+                while(!queue.isEmpty() && nums[(Integer)queue.peekLast()] <= nums[i]) {
+                    queue.pollLast();
+                }
+
+                queue.addLast(i);
+                if (!queue.isEmpty() && (Integer)queue.peek() <= i - size) {
+                    queue.poll();
+                }
+
+                if (i + 1 >= size && !queue.isEmpty()) {
+                    result[i + 1 - size] = nums[(Integer)queue.peek()];
+                }
+            }
+
+            return result;
+        } else {
+            return new double[0];
+        }
+    }
+
+    public static double[] averageSlidingWindow(double[] nums, int size) {
+        if (nums != null && nums.length > 0 && size > 0) {
+            double[] result = new double[nums.length - size + 1];
+            Queue<Double> qu = new LinkedList();
+            int ind = 0;
+
+            for(double num : nums) {
+                if (qu.size() == size) {
+                    result[ind++] = qu.stream().mapToDouble((value) -> value).average().getAsDouble();
+                    result[ind] = round(result[ind]);
+                    qu.poll();
+                    qu.offer(num);
+                } else {
+                    qu.offer(num);
+                }
+            }
+
+            result[ind] = qu.stream().mapToDouble((value) -> value).average().getAsDouble();
+            result[ind] = round(result[ind]);
+            return result;
+        } else {
+            return new double[0];
+        }
+    }
+
+    public static List<Double> averageByChunk(List<Double> list, int chunkSize) {
+        if (list == null || list.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        int size = list.size();
+        int chunks = (int) Math.ceil((double) size / chunkSize);
+        List<Double> result = new ArrayList<>(chunks);
+
+        for (int i = 0; i < chunks; i++) {
+            int start = i * chunkSize;
+            int end = Math.min(start + chunkSize, size);
+
+            double average = list.subList(start, end)
+                    .stream()
+                    .mapToDouble(Double::doubleValue)
+                    .average()
+                    .orElse(0.0);
+            average = round(average);
+
+            result.add(average);
+        }
+
+        return result;
+    }
+}
--- src/test/java/com/alipay/model/histogram/CpuMockSimpleAvgTest.java
+++ src/test/java/com/alipay/model/histogram/CpuMockSimpleAvgTest.java
@@ -1,22 +1,18 @@
 package com.alipay.model.histogram;
 
-import com.alipay.histogram.DecayingHistogram;
 import com.alipay.histogram.Histogram;
 import com.alipay.histogram.HistogramAggPolicy;
 import com.alipay.histogram.HistogramAggType;
 import com.alipay.histogram.HistogramOptions;
 import com.alipay.metrics.BaseMetric;
-import com.alipay.utils.AlgorithmUtil;
 import com.alipay.utils.FileUtil;
 import com.alipay.utils.TimestampUtil;
 import org.junit.Test;
 
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.alipay.consts.HistogramConsts.defaultEpsilon;
 import static com.alipay.consts.HistogramConsts.defaultHistogramBucketSizeGrowth;
@@ -28,7 +24,7 @@ import static com.alipay.consts.HistogramConsts.defaultHistogramBucketSizeGrowth
  * @date 12/31/25
  */
 
-public class CpuMockHistogramNodecayTest {
+public class CpuMockSimpleAvgTest {
 
     @Test
     public void testWithCpuHistogram() throws Exception {
@@ -66,46 +62,5 @@ public class CpuMockHistogramNodecayTest {
             }
         }
         FileUtil.writeFileFromList(outputPath, outputList);
-    }
-
-    @Test
-    public void testWithSingleSliding() throws Exception{
-        // exp variable
-        String filePath = "showdata2/cpumock2.csv";
-        String outputPath = "showdata2/cpumock2_mw_6.csv";
-        int sampleWindow = 12;
-        HistogramAggType type = HistogramAggType.AVERAGE;
-
-        List<BaseMetric> metricsList = new ArrayList<>();
-        FileUtil.readFileWithBufferedReader(filePath, line -> {
-            String[] split = line.split(",");
-            Long ts = TimestampUtil.stringToTimestampMs(split[0].trim());
-            Double value = Double.parseDouble(split[1].trim());
-            BaseMetric point = new BaseMetric("cpu_util", ts, value);
-            metricsList.add(point);
-        }, true);
-
-        List<Double> originValues = metricsList.stream().map(BaseMetric::getValue).toList();
-        double[] perHourValues = AlgorithmUtil.averageByChunk(originValues,20).stream().mapToDouble(Double::doubleValue).toArray();
-        double[] calValues = AlgorithmUtil.averageSlidingWindow(perHourValues, 12);// Support half day to cal avg, pedding is 1h
-        System.out.println(Arrays.toString(calValues));
-
-        LinkedList<String> outputList = new LinkedList<>();
-        outputList.addFirst("time,cpu_util");
-        //
-        //int lineNum = 0;
-        //for (BaseMetric baseMetric : metricsList) {
-        //    lineNum++;
-        //    if (lineNum % sampleWindow == 0) {
-        //    }
-        //}
-        //FileUtil.writeFileFromList(outputPath, outputList);
-    }
-
-    @Test
-    public void testSlidingUtils() {
-        double[] values = new double[]{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
-        double[] rs = AlgorithmUtil.averageSlidingWindow(values, 5);
-        System.out.println(Arrays.toString(rs));
     }
 }
