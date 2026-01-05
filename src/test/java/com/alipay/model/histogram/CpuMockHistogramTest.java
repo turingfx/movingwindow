f84a249b7bfdf71f85b83bd12fa3cdd0fcd934b0
杉思 <sansi.xy@antgroup.com>
2026-01-05 11:18:36 +0800

    fix

--- src/test/java/com/alipay/model/histogram/CpuMockHistogramTest.java
+++ src/test/java/com/alipay/model/histogram/CpuMockHistogramTest.java
@@ -28,8 +28,8 @@ public class CpuMockHistogramTest {
     public void testWithCpuHistogram() throws Exception {
         // exp variable
         String filePath = "showdata2/cpumock2.csv";
-        String outputPath = "showdata2/cpumock2_mw_2.csv";
-        long millis = Duration.ofHours(2).toMillis();
+        String outputPath = "showdata2/cpumock2_mw_4.csv";
+        long millis = Duration.ofMinutes(30).toMillis();
 
         HistogramOptions cpuHistogramOptions = new HistogramOptions(14000, 1, 1 + defaultHistogramBucketSizeGrowth, defaultEpsilon);
         DecayingHistogram decayingHistogram = new DecayingHistogram(cpuHistogramOptions, millis,
