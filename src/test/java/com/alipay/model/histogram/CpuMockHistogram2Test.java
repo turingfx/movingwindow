2f01c9c6901b4be098aab5d61707fb13506c1696
杉思 <sansi.xy@antgroup.com>
2026-01-05 11:28:52 +0800

    fix

---
+++ src/test/java/com/alipay/model/histogram/CpuMockHistogram2Test.java
@@ -0,0 +1,65 @@
+package com.alipay.model.histogram;
+
+import com.alipay.histogram.DecayingHistogram;
+import com.alipay.histogram.HistogramOptions;
+import com.alipay.metrics.BaseMetric;
+import com.alipay.utils.FileUtil;
+import com.alipay.utils.TimestampUtil;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.alipay.consts.HistogramConsts.defaultEpsilon;
+import static com.alipay.consts.HistogramConsts.defaultHistogramBucketSizeGrowth;
+
+/**
+ * Mock with Cpu util data
+ *
+ * @author sansi.xy
+ * @date 12/31/25
+ */
+
+public class CpuMockHistogram2Test {
+
+    @Test
+    public void testWithCpuHistogram() throws Exception {
+        // exp variable
+        String filePath = "showdata2/cpumock2.csv";
+        String outputPath = "showdata2/cpumock2_mw_3.csv";
+        long millis = Duration.ofMinutes(60).toMillis();
+        int sampleWindow = 12;
+
+        HistogramOptions cpuHistogramOptions = new HistogramOptions(14000, 1, 1 + defaultHistogramBucketSizeGrowth, defaultEpsilon);
+        DecayingHistogram decayingHistogram = new DecayingHistogram(cpuHistogramOptions, millis,
+                TimestampUtil.stringToTimestampMs("2025-12-29 11:10:00"));
+
+        List<BaseMetric> metricsList = new ArrayList<>();
+        FileUtil.readFileWithBufferedReader(filePath, line -> {
+            String[] split = line.split(",");
+            Long ts = TimestampUtil.stringToTimestampMs(split[0].trim());
+            Double value = Double.parseDouble(split[1].trim());
+            BaseMetric point = new BaseMetric("cpu_util", ts, value);
+            metricsList.add(point);
+        }, true);
+
+        LinkedList<String> outputList = new LinkedList<>();
+        outputList.addFirst("time,cpu_util");
+
+        int lineNum = 0;
+        for (BaseMetric baseMetric : metricsList) {
+            lineNum++;
+            if (lineNum % sampleWindow == 0) {
+                decayingHistogram.addSample(baseMetric.getValue(), 1, baseMetric.getTimestamp());
+                String str = TimestampUtil.timestampMsToString(baseMetric.getTimestamp());
+                outputList.add(str
+                        + ","
+                        + decayingHistogram.average()
+                );
+            }
+        }
+        FileUtil.writeFileFromList(outputPath, outputList);
+    }
+}
